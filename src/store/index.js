ccb9040826e8132ed0ff98dacc36477228aaa1ca
danyelhenrique <danyelhenriquefidel@gmail.com>
2019-12-29 23:46:05 -0300

    --fix apollo config to ssr with next

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,10 @@ import { makeExecutableSchema } from 'graphql-tools';
 import { productMutation } from './mutation';
 import { productQuery } from './query';
 import { productType } from './types';
-// import resolvers from './resolvers';
 
 const typeDefs = [ Root, productQuery, productMutation, productType ];
 
 const schema = makeExecutableSchema({
 	typeDefs
-	// resolvers,
 });
 export default schema;
